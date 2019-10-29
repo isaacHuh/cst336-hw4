d4e09a2e78e66f80896b4e340fe56f099a75c640
EC2 Default User <ec2-user@ip-172-31-31-62.us-west-2.compute.internal>
2019-10-29 03:03:15 +0000

    changes

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const faker = require("faker");
+const app = express();
+
+app.engine('html',require('ejs').renderFile);
+app.use(express.static("public"));
+
+console.log(faker.name.findName());
+console.log(faker.image.avatar());
+
+//routes
+app.get("/", function(req,res){
+   res.render("index.ejs"); 
+});
+
+app.get("/iloveyou", function(req,res){
+   res.render("iloveyou.ejs"); 
+});
+
+app.get("/sasser", function(req,res){
+   res.render("sasser.ejs"); 
+});
+
+app.get("/wannacry", function(req,res){
+   res.render("wannacry.ejs"); 
+});
+
+app.get("/faker", function(req,res){
+   res.render("faker.ejs", {
+      virusName: faker.internet.userName(),
+      randImage: faker.image.cats(),
+      randText: faker.lorem.paragraphs(),
+      randNum: faker.random.number(),
+      randUrl: faker.image.sports()
+   }); 
+});
+
+//starting server
+app.listen(process.env.PORT, process.env.IP, function(){
+console.log("Express server is running...");
+});
